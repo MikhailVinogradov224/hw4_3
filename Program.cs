cf0169d1756a8861955e089bf3b380f0a3e766dd
Mikhail Vinogradov <m89119750476@gmail.com>
2022-05-21 23:00:14 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿            string a;
+            int[] b = new int[100];
+            int i=0;
+ 
+            while ((a = Console.ReadLine()) != "end");
+                b[i++] = Convert.ToInt32(a); 
+ 
+            for (int k = 0; k < i; k++) 
+                Console.Write(b[k]);
+ 
+            Console.ReadKey(true);
